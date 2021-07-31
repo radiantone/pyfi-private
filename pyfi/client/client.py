6433f12c9a4fea2bab9525a8ee8214003b157f0b
Darren Govoni <darren@rapidquest.ai>
2021-07-31 16:03:46 -0400

    Test client

---
+++ pyfi/client/client.py
@@ -0,0 +1,12 @@
+from pyfi.processor import Processor
+
+processor = Processor('pyfi.queue1', 'pyfi.processors.sample.do_something')
+
+result1 = processor('Hello World!')
+#print("Calling processor 2")
+#result2 = processor('It\'s me!')
+
+print(result1.get())
+
+
+#print(result2.get())
