62bd88874dd9956fa8b16d4a93487d5a9adc0dc8
Darren Govoni <darren@rapidquest.ai>
2021-08-10 15:08:54 -0400

    Deleted

--- pyfi/tt.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-
-celery = Celery('pyfi', backend='redis://192.168.1.23',
-                broker='pyamqp://192.168.1.23')
-
-@celery.task
-def see():
-   print("I see TT!")
-   return "I see TT!"
