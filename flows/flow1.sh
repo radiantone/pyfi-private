355210cfc8a7d0d4413061ec0536d10fb1cc6486
Darren Govoni <darren@rapidquest.ai>
2021-08-10 15:04:34 -0400

    Minor update

--- flows/flow1.sh
+++ flows/flow1.sh
@@ -1,13 +1,27 @@
+
+pyfi add node -n node1 -h phoenix
+pyfi add node -n node2 -h radiant 
+pyfi add node -n node3 -h miko
+pyfi add scheduler --name sched1
+
+pyfi scheduler -n sched1  add --node node1
+pyfi scheduler -n sched1  add --node node2
+pyfi scheduler -n sched1  add --node node3
+
 pyfi add processor -n proc1 -g https://github.com/radiantone/pyfi-processors -m pyfi.processors.sample -t do_something
 pyfi add queue -n pyfi.queue1 -t direct
 pyfi add queue -n pyfi.queue2 -t direct
-pyfi add outlet -n outlet1 -q pyfi.queue1 -pn proc1
+pyfi add queue -n pyfi.queue3 -t direct
+
+pyfi add outlet -n proc1.outlet1 -q pyfi.queue1 -pn proc1
 pyfi add plug -n plug1 -q pyfi.queue2 -pn proc1
+pyfi add plug -n plug3 -q pyfi.queue3 -pn proc1
 pyfi add processor -n proc2 -g https://github.com/radiantone/pyfi-processors -m pyfi.processors.sample -t do_this -h radiant
-pyfi add outlet -n outlet1 -q pyfi.queue2 -pn proc2
+pyfi add outlet -n proc2.outlet1 -q pyfi.queue2 -pn proc2
 
 pyfi add processor -n proc4 -g https://github.com/radiantone/pyfi-processors -m pyfi.processors.sample -t do_something -h radiant
-pyfi add outlet -n outlet1 -q pyfi.queue1 -pn proc4
+pyfi add outlet -n proc4.outlet1 -q pyfi.queue1 -pn proc4
 
 pyfi add processor -n proc3 -g https://github.com/radiantone/pyfi-processors -m pyfi.processors.sample -t do_this -h miko
-pyfi add outlet -n outlet1 -q pyfi.queue2 -pn proc3
+#pyfi add outlet -n proc3.outlet1 -q pyfi.queue2 -pn proc3
+pyfi add outlet -n proc3.outlet1 -q pyfi.queue3 -pn proc3
