8955aeabcab4574a7c3dab17aae07b7c383c35ae
Darren Govoni <darren@rapidquest.ai>
2021-08-10 15:09:59 -0400

    Ctrl-C and graceful shutdown handling

--- pyfi/worker/worker.py
+++ pyfi/worker/worker.py
@@ -6,7 +6,9 @@ import shutil
 import os
 import sys
 import psutil
+import signal
 
+from multiprocessing import Condition, Queue
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -17,12 +19,33 @@ from celery import Celery
 from celery.signals import worker_process_init, after_task_publish, task_success, task_prerun, task_postrun, task_failure, task_internal_error, task_received
 
 
+import socketio
+
+
+lock = Condition()
+queue = Queue()
+
+global processes
+processes = []
+
+
+def shutdown(*args):
+    for process in processes:
+        print("Terminating ", process.pid)
+        os.kill(process.pid, signal.SIGKILL)
+        process.terminate()
+        
+    exit(0)
+    
+
+signal.signal(signal.SIGINT, shutdown)
+
 class Worker:
     """
     A worker is a celery worker with a processor module loaded and represents a single processor
     """
 
-    def __init__(self, processor, workdir, pool=4, database=None, backend='redis://192.168.1.23', config=None, broker='pyamqp://192.168.1.23'):
+    def __init__(self, processor, workdir, pool=4, database=None, backend='redis://phoenix', config=None, broker='pyamqp://phoenix'):
         """
         """
         from pyfi.db.model import Base
@@ -32,8 +55,11 @@ class Worker:
         self.backend = backend
         self.broker = broker
         self.workdir = workdir
-        self.database = database
-        self.dburi = database.uri
+        self.dburi = database
+
+        self.database = create_engine(self.dburi)
+        self.session = sessionmaker(bind=self.database)()
+        self.database.session = self.session
         self.pool = pool
 
         if config is not None:
@@ -46,24 +72,29 @@ class Worker:
         else:
             self.celery = celery = Celery(
                 'pyfi', backend=backend, broker=broker)
-        self.process = None
-        logging.info("Starting worker with pool[{}] backend:{} broker:{}".format(pool, backend, broker))
 
+        self.process = None
+        logging.info("Starting worker with pool[{}] backend:{} broker:{}".format(
+            pool, backend, broker))
 
     def start(self):
         """
         Docstring
         """
+        global processes
         from multiprocessing import Process
         import os
 
-        def worker_proc(app):
+        def worker_proc(app, _queue):
             """ Set up celery queues for self.celery """
             import builtins
             import importlib
             import sys
+            import json
+            import time
+
             from billiard.pool import Pool
-            
+
             logging.info("Processor beat: %s", self.processor.beat)
 
             queues = []
@@ -72,11 +103,33 @@ class Worker:
             session = sessionmaker(bind=engine)()
             self.processor = session.query(
                 ProcessorModel).filter_by(id=self.processor.id).first()
-                
+
+            sio = socketio.Client()
+
+            @sio.on('queue', namespace='/tasks')
+            def message(data):
+                print('I received a message!', data)
+
+            @sio.on('connect', namespace='/tasks')
+            def connect():
+                logging.info("I'm connected to namespace /tasks!")
+
+                sio.emit('servermsg', {
+                    'module': self.processor.module, 'task': self.processor.task}, namespace='/tasks')
+
+            while True:
+                try:
+                    sio.connect('http://localhost:5000',
+                                namespaces=['/tasks'])
+                    break
+                except Exception as ex:
+                    logging.error(ex)
+                    time.sleep(3)
+
             if self.processor and self.processor.outlets and len(self.processor.outlets) > 0:
                 for outlet in self.processor.outlets:
                     if outlet.queue:
-                        print("queue: ",outlet.queue)
+                        print("queue: ", outlet.queue)
                         queues += [outlet.queue.name+'.'+self.processor.name]
                         queues += [outlet.queue.name+'.topic']
                         from kombu import Exchange, Queue, binding
@@ -90,7 +143,6 @@ class Worker:
                                 'exchange': [outlet.queue.name+'.topic', outlet.queue.name]
                             }
                         }
-
 
             @worker_process_init.connect()
             def prep_db_pool(**kwargs):
@@ -114,7 +166,6 @@ class Worker:
                 concurrency=int(self.processor.concurrency)
             )
 
-            
             if self.processor.beat:
                 worker.app.conf.beat_schedule = {
                     "run-me-every-ten-seconds": {
@@ -126,7 +177,7 @@ class Worker:
 
             sys.path.append(os.getcwd())
 
-            setattr(builtins,'worker',worker)
+            setattr(builtins, 'worker', worker)
 
             module = importlib.import_module(self.processor.module)
             func = getattr(module, self.processor.task)
@@ -136,7 +187,8 @@ class Worker:
                 _plugs[plug.queue.name] = []
 
             # Processor properties are set on the task here
-            func = self.celery.task(func, name=self.processor.module+'.'+self.processor.task, retries=self.processor.retries)
+            func = self.celery.task(func, name=self.processor.module +
+                                    '.'+self.processor.task, retries=self.processor.retries)
 
             @task_prerun.connect(sender=func)
             def pyfi_task_prerun(sender=None, **kwargs):
@@ -165,48 +217,104 @@ class Worker:
                 task_kwargs = kwargs.get('kwargs')
                 plugs = task_kwargs['plugs']
 
-                for key in plugs:
+                try:
+                    while _queue.qsize() > 1000:
+                        loggin.info("Waiting for queue to shrink")
+                        time.sleep(0.5)
 
-                    processor_plug = None
-                    for _plug in self.processor.plugs:
-                        if _plug.queue.name == key:
-                            processor_plug = _plug
+                    _queue.put(['servermsg', {
+                               'module': self.processor.module, 'task': self.processor.task}])
+                    print("PLUGS:", plugs)
+                    for key in plugs:
 
-                    processors = self.database.session.query(
-                        ProcessorModel).filter(ProcessorModel.outlets.any(OutletModel.queue.has(name=key)))
+                        processor_plug = None
 
-                    for msg in plugs[key]:
-                        logging.info("Sending {} to queue {}".format(msg, key))
+                        for _plug in self.processor.plugs:
+                            if _plug.queue.name == key:
+                                processor_plug = _plug
 
-                        if processor_plug.queue.qtype == 'direct':
-                            for processor in processors:
-                                print("Invoking {}=>{}.{}({})".format(
-                                    key, processor.module, processor.task, msg))
+                        processors = self.database.session.query(
+                            ProcessorModel).filter(ProcessorModel.outlets.any(OutletModel.queue.has(name=key)))
 
-                                # Target specific worker queue here
-                                worker_queue = key+'.'+processor.name
-                                self.celery.signature(
-                                    processor.module+'.'+processor.task, args=(msg,), queue=worker_queue, kwargs={}).delay()
-            
+                        for msg in plugs[key]:
+                            logging.info(
+                                "Sending {} to queue {}".format(msg, key))
+
+                            if processor_plug.queue.qtype == 'direct':
+                                for processor in processors:
+                                    print("Invoking {}=>{}.{}({})".format(
+                                        key, processor.module, processor.task, msg))
+
+                                    # Target specific worker queue here
+                                    worker_queue = key+'.'+processor.name
+                                    self.celery.signature(
+                                        processor.module+'.'+processor.task, args=(msg,), queue=worker_queue, kwargs={}).delay()
+                except:
+                    import traceback
+                    print(traceback.format_exc())
+                    pass
+
             worker.start()
 
         logging.info("Preparing worker %s %s %s %s %s", self.worker.name,
                      self.processor.plugs, self.backend, self.broker, self.worker.processor.module)
 
         os.chdir(self.workdir)
-        
+        import time
+
         if self.processor.gitrepo:
             logging.info("git clone -b {} --single-branch {} git".format(
                 self.processor.branch, self.processor.gitrepo))
             shutil.rmtree("git", ignore_errors=True)
-            os.system(
-                "git clone -b {} --single-branch {} git".format(self.processor.branch, self.processor.gitrepo))
-            sys.path.append(self.workdir+'/git')
-            os.chdir('git')
 
-        process = Process(target=worker_proc, args=(self.celery,))
+            while True:
+                try:
+                    os.system(
+                        "git clone -b {} --single-branch {} git".format(self.processor.branch, self.processor.gitrepo))
+                    sys.path.append(self.workdir+'/git')
+                    os.chdir('git')
+                    break
+                except Exception as ex:
+                    logging.error(ex)
+                    time.sleep(3)
+
+        process = Process(target=worker_proc, args=(self.celery, queue))
+        processes += [process]
         process.start()
+        print("PROCESS PID",process.pid)
         self.process = process
+
+        def emit_messages():
+            import json
+
+            sio = socketio.Client()
+
+            while True:
+                try:
+                    sio.connect('http://localhost:5000',
+                                namespaces=['/tasks'])
+                    break
+                except Exception as ex:
+                    logging.error(ex)
+                    time.sleep(3)
+
+            last_qsize = 0
+            while True:
+                try:
+                    print("QSIZE: ", queue.qsize())
+                    message = queue.get()
+                    print("EMITTING: ", message)
+                    sio.emit(*message, namespace='/tasks')
+                except Exception as ex:
+                    logging.error(ex)
+                    time.sleep(3)
+
+        process2 = Process(target=emit_messages)
+        process2.daemon = True
+        #print("PROCESS2 PID", process2.pid)
+        #processes += [process2]
+        process2.start()
+
         logging.info("Started worker process with pid[%s]", process.pid)
         return process
 
@@ -215,7 +323,7 @@ class Worker:
         Docstring
         """
         #cinspect = celery.current_app.control.inspect()
-        #cinspect.active()) + ccount(cinspect.scheduled()) + ccount(cinspect.reserved())
+        # cinspect.active()) + ccount(cinspect.scheduled()) + ccount(cinspect.reserved())
         pass
 
     def suspend(self):
