f338fd9f092f854fa0760b153d6deb72b3fb68c9
Darren Govoni <darren@rapidquest.ai>
2021-08-10 15:11:05 -0400

    Replace IP address with names

--- pyfi/config/celery.py
+++ pyfi/config/celery.py
@@ -1,11 +1,13 @@
 """
 --config pyfi.conf.celery.Config
 """
+
+
 class Config:
     """
-    
+
     """
     enable_utc = True
     timezone = 'America/New_York'
-    broker_url = 'pyamqp://192.168.1.23'
-    result_backend = None #'redis://192.168.1.23'
+    broker_url = 'pyamqp://phoenix'
+    result_backend = None  # 'redis://phoenix'
