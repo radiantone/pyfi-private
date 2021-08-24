f63255cf4e43d502492d7959426aea71b51782b3
Darren Govoni <darren@rapidquest.ai>
2021-08-24 09:48:43 -0400

    Added listen command and log streaming with rooms/namespaces

--- pyfi/server/events.py
+++ pyfi/server/events.py
@@ -1,19 +1,35 @@
 
-import eventlet
 import socketio
 import uvicorn
-
+import json
 
 sio = socketio.AsyncServer(async_mode='asgi')
 app = socketio.ASGIApp(sio)
 
+'''
+Workers will send messages to specific rooms such as pyfi.queue1.proc1 with messages
+for that processor name. invoke pyfi listen pyfi.queue1.proc1 --namespace tasks will listen
+to messages on that channel.
+
+processor socket tasks (functions) can post messages to the room which can be spotted by
+pyfi components such as logs, charts and graphs for real-time visualization
+'''
+
 
 @sio.on('servermsg', namespace='/tasks')
-async def message(sid, data):
+async def server_message(sid, data):
     print("GOT MESSAGE", data)
     await sio.emit('queue', data,
                    namespace='/tasks')
 
+    #await sio.emit('task', data, room='pyfi.queue1.proc1',  namespace='/tasks')
+    #print("Sent to task")
+
+@sio.on('roomsg', namespace='/tasks')
+async def room_message(sid, data):
+    print("ROOM MESSAGE", data)
+    #await sio.emit(_data['channel'], _data['message'], skip_sid=sid)
+    await sio.emit(data['channel'], data['message'], room=data['room'],  namespace='/tasks')
 
 @sio.on('connect', namespace='/tasks')
 async def connect(sid, environ):
@@ -24,17 +40,18 @@ async def connect(sid, environ):
     print("Entered lobby")
 
 
-@sio.event(namespace='/tasks')
-def my_message(sid, data):
-    print('message ', data)
+@sio.on('join', namespace='/tasks')
+async def on_join(sid, data):
+    channel = data['room']
+    print("Entering room", channel)
+    sio.enter_room(sid, channel, namespace='/tasks')
 
 
 @sio.event
-def disconnect(sid):
+async def disconnect(sid):
     print('disconnect ', sid)
 
 
 if __name__ == '__main__':
-    #eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
     uvicorn.run("pyfi.server.events:app", host="0.0.0.0",
                 port=5000, log_level="info")
