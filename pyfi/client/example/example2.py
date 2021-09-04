90002dbcff0a43b9296d99123316fc0737da514a
Darren Govoni <darren@rapidquest.ai>
2021-09-04 19:32:01 -0400

    Updates

--- pyfi/client/example/example2.py
+++ pyfi/client/example/example2.py
@@ -1,6 +1,15 @@
 from pyfi.client.example.api import do_something
+from celery import chord
 
 # Send a message to the socket function
 result = do_something("Inner "+" ".join(do_something("Hello World XXX!")))
 
 print("Result is: ",result)
+
+counting = chord([
+    do_something.p("One"), 
+    do_something.p("Two"), 
+    do_something.p("Three") ])
+
+
+print("COUNTING: ", counting())
