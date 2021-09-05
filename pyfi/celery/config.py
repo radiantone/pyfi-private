bfb205d61416c013f2ace6006c7bc39da0c6bcf7
Darren Govoni <darren@rapidquest.ai>
2021-09-05 00:13:29 -0400

    Updates

--- pyfi/celery/config.py
+++ pyfi/celery/config.py
@@ -1,9 +1,7 @@
 from kombu import serialization
-
-accept_content = ['pickle', 'application/x-python-serialize']
+accept_content = ['json', 'pickle',
+                  'application/json', 'application/x-python-serialize']
 task_serializer = 'pickle'
 result_serializer = 'pickle'
-
 serialization.register_pickle()
 serialization.enable_insecure_serializers()
-print("CONFIG DONE")
