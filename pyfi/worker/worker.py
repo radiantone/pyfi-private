f454b140b6d17c29bd5f9d6f3859669f0f3f90a4
Darren Govoni <darren@rapidquest.ai>
2021-09-12 05:35:10 -0400

    Updates

--- pyfi/worker/worker.py
+++ pyfi/worker/worker.py
@@ -733,7 +733,7 @@ class Worker:
 
                         @task_received.connect()
                         def pyfi_task_received(sender=None, request=None, **kwargs):
-                            logging.info("Task RECEIVED %s", sender)
+                            logging.info("Task RECEIVED %s %s", request.id, sender)
                             logging.info("Task Request Parent %s", request.parent_id)
                             pass
 
