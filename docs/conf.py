f78dd1023085236a8d233f9fac75426199697d6e
Darren Govoni <darren@rapidquest.ai>
2021-09-14 10:05:22 -0400

    Updates

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'PYFI'
-copyright = '2021, Observable Data Inc'
+copyright = '2021, Darren Govoni'
 author = 'Darren Govoni'
 
 # The full version, including alpha/beta/rc tags
@@ -30,8 +30,8 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx_click.ext']
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
--- examples/short.sh
+++ examples/short.sh
@@ -1,5 +1,5 @@
 pyfi add queue -n pyfi.queue1 -t direct
-pyfi add processor -n proc1 -g https://github.com/radiantone/pyfi-processors -m pyfi.processors.sample 
+pyfi add processor -n proc1 -g https://radiantone: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@github.com/radiantone/pyfi-processors#egg=pyfi-processor -m pyfi.processors.sample -h phoenix
 pyfi add socket -n proc1.socket1 -q pyfi.queue1 -pn proc1 -t do_something
 pyfi add socket -n proc1.socket2 -q pyfi.queue1 -pn proc1 -t do_this
 pyfi task run --socket proc1.socket2 --data "['some data']"
