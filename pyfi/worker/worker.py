8172ae14b9aab95f1871c192e852f2905ac25e6a
Darren Govoni <darren@rapidquest.ai>
2021-09-22 06:04:29 -0400

    Updates

--- pyfi/worker/worker.py
+++ pyfi/worker/worker.py
@@ -631,7 +631,7 @@ class Worker:
                                         delayed = pipeline(
                                             plug_sig,
                                             task_sig
-                                        )
+                                        ).delay()
                                         pipelines += [delayed]
 
                                         #logging.info(
@@ -649,7 +649,7 @@ class Worker:
                                     plugs[pname].remove(msg)
 
                         # Execute parallel( pipeline(plug,task), ...)
-                        delayed = parallel(*pipelines).delayed()
+                        delayed = parallel(*pipelines).delay()
                         logging.info("PARALLEL invoke %s",delayed)
                         result = delayed.get()
                         logging.info("PARALLEL result %s",result)
