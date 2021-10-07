9121aca8b12d03999a8e561bbf1c3fa4bbdd089a
Darren Govoni <darren@rapidquest.ai>
2021-10-07 10:51:18 -0400

    Updates

--- bin/docker-refresh.sh
+++ bin/docker-refresh.sh
@@ -1,2 +1,2 @@
-docker ps -a|awk '{ print "docker stop "$1}'|sh
-docker ps -a|awk '{ print "docker rm "$1}'|sh
+docker ps -a|grep -v CONTAINER|awk '{ print "docker stop "$1}'|sh
+docker ps -a|grep -v CONTAINER|awk '{ print "docker rm "$1}'|sh
