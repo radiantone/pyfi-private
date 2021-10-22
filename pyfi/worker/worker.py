355b83875dbe6b33a1bbd804e4d6e617cb90aa8d
Darren Govoni <darren@rapidquest.ai>
2021-10-22 08:11:32 -0400

    Updates

--- pyfi/worker/worker.py
+++ pyfi/worker/worker.py
@@ -563,6 +563,7 @@ class Worker:
                             processor_plug = None
 
                             if pname not in sourceplugs:
+                                logging.info("%s plug not in %s", pname, sourceplugs)
                                 continue
 
                             processor_plug = sourceplugs[pname]
