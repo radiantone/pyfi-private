85c9aedfb68d15933cabc78922b1b50b01b745e6
Darren Govoni <darren@rapidquest.ai>
2021-12-01 15:08:44 -0500

    Updates

--- pyfi/yaml/builder.py
+++ pyfi/yaml/builder.py
@@ -22,7 +22,7 @@ def install_repo(path, ini, polar, hostname, username, sshkey, branch, pyfi, rep
 
     # Kill existing processors and remove existing directories
     # ps -ef|grep pyfi|awk '{ print "kill "$2 }'|sh
-    command = "ps -ef|grep pyfi|awk '{print \"kill \"$2}'|sh"
+    command = "ps -ef|grep pyfi|grep -v 'pyfi build'|awk '{print \"kill \"$2}'|sh"
     logging.info(hostname+":"+command)
     _, stdout, stderr = _ssh.exec_command(command)
     command = "ps -ef|grep celery|awk '{print \"kill \"$2}'|sh"
