b635133ba4087920542c70af353acfe8ded6e6c8
Darren Govoni <darren@rapidquest.ai>
2021-12-06 04:41:15 -0500

    Updates

--- pyfi/yaml/builder.py
+++ pyfi/yaml/builder.py
@@ -25,10 +25,10 @@ def install_repo(path, ini, polar, hostname, username, sshkey, branch, pyfi, rep
         sftp.put(polar, '/home/'+username+'/pyfi.polar')
 
     agent = Agent.find(name=hostname+'.agent')
-    command = 'kill -s SIGINT '+agent.pid
 
     # Kill any existing agent
     if agent and agent.pid:
+        command = 'kill -s SIGINT '+agent.pid
         _, stdout, stderr = _ssh.exec_command(command)
 
     # Kill existing processors and remove existing directories
