644a7a7571523bce100e0c894e35ddf2cdbdd731
Darren Govoni <darren@rapidquest.ai>
2021-12-13 21:52:45 -0500

    Updates

--- pyfi/worker/worker.py
+++ pyfi/worker/worker.py
@@ -912,6 +912,7 @@ class Worker:
                                         'x-message-ttl': 30000,
                                         'x-expires': 300
                                     }
+                                )
                             ]
 
                             task_queues += [
