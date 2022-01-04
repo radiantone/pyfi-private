3232f853dd344d1e7ec221cada0591ba0d0e77a6
Darren Govoni <darren@rapidquest.ai>
2022-01-04 18:46:07 -0500

    Updates

--- pyfi/agent/agent.py
+++ pyfi/agent/agent.py
@@ -593,8 +593,8 @@ class Agent:
                                     logging.info("Deployment worker %s",deployment)
                                     # Only launch worker if we have a deployment for our host
                                     if deployment.hostname == HOSTNAME:
-                                        logging.info("Deployment hostname is {} and HOSTNAME is {} {}".format(
-                                            deployment.hostname, HOSTNAME, os.environ['PYFI_HOSTNAME']))
+                                        logging.info("Deployment hostname is {} and HOSTNAME is {}".format(
+                                            deployment.hostname, HOSTNAME))
                                         processor['deployment'] = deployment
                                         logging.info("-------------------------------------------------------")
                                         logging.info(
