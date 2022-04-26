45258f22da597d425d3ee61dd7de7ac97d5d0b0a
Darren Govoni <darren@rapidquest.ai>
2022-04-26 19:37:51 -0400

    Updates

--- pyfi/agent/_agent2.py
+++ pyfi/agent/_agent2.py
@@ -330,6 +330,7 @@ class AgentMonitorPlugin(AgentPlugin):
                     '''
 
                     if "worker" in processor and processor["worker"] and "model" in processor["worker"]:
+                        session.merge(processor["worker"]["model"], load=True)
                         session.refresh(processor["worker"]["model"])
 
                     print("WORKER",processor["worker"])
