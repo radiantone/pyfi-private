e88dd7487a25b3f4c8cbd06fbb0c4df3ed85b4fb
Darren Govoni <darren@rapidquest.ai>
2022-04-26 18:19:20 -0400

    Updates

--- pyfi/agent/_agent2.py
+++ pyfi/agent/_agent2.py
@@ -522,7 +522,7 @@ class AgentMonitorPlugin(AgentPlugin):
 
                         if "deployment" in processor:
                             print("DEPLOYMENT",processor["deployment"])
-                            session.merge(processor["deployment"])
+                            session.add(processor["deployment"])
                             print("DEPLOYMENT.WORKER",processor["deployment"].worker)
 
                         """
