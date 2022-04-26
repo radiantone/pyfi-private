14ac458c6351c72b6c33b26ab6181736c7a1621e
Darren Govoni <darren@rapidquest.ai>
2022-04-26 14:43:24 -0400

    Updates

--- pyfi/agent/_agent.py
+++ pyfi/agent/_agent.py
@@ -883,7 +883,7 @@ class AgentMonitorPlugin(AgentPlugin):
 
             logging.info("AgentMonitorPlugin: agent cpus %s",agent.cpus)
         # Create a list of pluggable "Monitor" objects that perform various independent tasks
-        monitors = [ProcessorMonitor(agent_service)] #, DeploymentMonitor(agent_service), NodeMonitor()]
+        monitors = [ProcessorMonitor(agent_service), DeploymentMonitor(agent_service)] #, DeploymentMonitor(agent_service), NodeMonitor()]
 
         for monitor in monitors:
             self.monitors[monitor.__class__.__name__] = monitor
