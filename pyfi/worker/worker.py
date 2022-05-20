15e4685f720e8386bcb7750112ca86be68dd6b62
Darren Govoni <darren@rapidquest.ai>
2022-05-20 13:19:06 -0400

    Updates

--- pyfi/worker/worker.py
+++ pyfi/worker/worker.py
@@ -2069,7 +2069,7 @@ class WorkerService:
                             )
 
                             start_time = run_times[task_id]
-                            duration = datetime.timedelta(seconds=time()-start_time)
+                            duration = datetime.timedelta(seconds=time.time()-start_time)
                             logging.info("POSTRUN PUTTING ON main_queue")
                             self.main_queue.put(
                                 {
