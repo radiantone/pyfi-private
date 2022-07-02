2e603af9e9793d08d54a6aad135484ef1c806923
Darren Govoni <darren@rapidquest.ai>
2022-07-02 06:33:36 -0400

    Updates

--- pyfi/cli.py
+++ pyfi/cli.py
@@ -1469,6 +1469,7 @@ def run_task(context, name, format, socket, data, nodata, argument, synchronized
                 return []
 
         p_calls = build_pipeline(socket)
+        print("P_CALLS",p_calls)
         p = pipeline([socket.p(_data), p_calls])
         print(p().get())
         return
