9fbb9a91a7ea4a943bf43692987d22100f68e628
Darren Govoni <darren@rapidquest.ai>
2022-07-28 11:28:39 -0400

    Updates

--- pyfi/client/example/example18.py
+++ pyfi/client/example/example18.py
@@ -1,9 +1,9 @@
 from pyfi.client.example.api import do_something, do_this
 
-print("do_something")
-result = do_something("HI!!!")
-print("Got result")
-print(result)
+#print("do_something")
+#result = do_something("HI!!!")
+#print("Got result")
+#print(result)
 print("do_this")
 result = do_this("HI!!!")
 print("Got result")
