9ef5169de9976fbff7d0967db3a52c10afb181f5
Darren Govoni <darren@rapidquest.ai>
2022-08-08 09:10:45 -0400

    Updates

--- pyfi/worker/worker.py
+++ pyfi/worker/worker.py
@@ -344,10 +344,10 @@ class WorkerService:
 
         if os.path.isabs(self.workdir):
             self.workpath = self.workdir
-            logging.info("Setting workpath to %s",self.workpath)
+            logging.info("Setting workpath to %s", self.workpath)
         else:
             self.workpath = self.basedir + "/" + self.workdir
-            logging.info("Setting workpath to %s",self.workpath)
+            logging.info("Setting workpath to basedir %s + workdir %s ", self.basedir, self.workdir)
 
         logging.debug("INIT WORKERSERVICE")
 
