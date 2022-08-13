29441253aca5881e4e755708aa3841acfd55b0fc
Darren Govoni <darren@rapidquest.ai>
2022-08-13 07:43:14 -0400

    Updates

--- pyfi/db/__init__.py
+++ pyfi/db/__init__.py
@@ -56,11 +56,21 @@ def receive_before_update(mapper, connection, target):
         logging.debug("No changes!")
 
 
+global session
+session = None
+
+
 def get_session():
     from sqlalchemy.orm import sessionmaker, scoped_session
+    from sqlalchemy.pool import NullPool
+    global session
 
-    _engine = create_engine(CONFIG.get("database", "uri"), isolation_level='READ UNCOMMITTED')
+    _engine = create_engine(CONFIG.get("database", "uri"), isolation_level="AUTOCOMMIT", poolclass=NullPool)
     _session = scoped_session(sessionmaker(autocommit=False, autoflush=True, bind=_engine))
+
+    if session:
+        return session
+    session = _session
 
     return _session
 
