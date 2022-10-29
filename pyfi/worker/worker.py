4fb021c5ccd53273226bee090c7a49f3de4e0c1c
Darren Govoni <darren@elasticcode.ai>
2022-10-29 02:05:50 -0400

    Updates

--- pyfi/worker/worker.py
+++ pyfi/worker/worker.py
@@ -134,8 +134,6 @@ def execute_function(taskid, mname, fname, *args, **kwargs):
     with open("/tmp/" + taskid + ".log2", "w") as logs:
         logs.write("Importing module "+str(mname)+" "+str(fname))
 
-    #logging.info("Execute function %s %s %s", taskid, mname, fname)
-
     _args = args
     _kwargs = kwargs
 
