61a1265291fd24778b8a2e26867937978d28094a
Darren Govoni <darren@elasticcode.ai>
2023-01-25 08:57:40 -0500

    Updates

--- bin/up.sh
+++ bin/up.sh
@@ -1,1 +1,1 @@
-docker compose up -f docker-compose-ecr.yml -d postgresdb redis rabbitmq rabbitmq2 websockets websockets2 nginx globalsocket clientsocket mongodb web api
+docker compose -f docker-compose-ecr.yml up -d postgresdb redis rabbitmq rabbitmq2 websockets websockets2 nginx globalsocket clientsocket mongodb web api
