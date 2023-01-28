4fe95a0a5b0907a1ea7a67f5ecc4fd7fd09316d1
Darren Govoni <darren@elasticcode.ai>
2023-01-28 09:27:05 -0500

    Updates

--- pyfi/server/api.py
+++ pyfi/server/api.py
@@ -29,7 +29,7 @@ from flask import (
     send_from_directory,
     session,
 )
-from flask_cors import cross_origin
+from flask_cors import cross_origin, CORS
 from flask_restx import Api, Resource, fields, reqparse
 from jose import JWTError, jwt
 from six.moves.urllib.request import urlopen
@@ -76,6 +76,7 @@ hostname = platform.node()
 app = Flask(__name__)
 app.secret_key = "super secret key"
 app.register_blueprint(blueprint)
+cors = CORS(app, resources={r"/*": {"origins": "*.elasticcode.ai"}})
 
 api = Api(
     app,
@@ -575,7 +576,7 @@ def get_result(resultid):
 
 
 @app.route("/files/<collection>/<path:path>", methods=["GET"])
-@cross_origin(headers=["Content-Type", "Authorization"])
+@cross_origin(origin='*', headers=["Content-Type", "Authorization"])
 @requires_auth
 def get_files(collection, path):
     import json
