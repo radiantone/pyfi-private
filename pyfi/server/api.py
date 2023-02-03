976d0fa2326324aaca76d3e73a4c741f93b66db7
Darren Govoni <darren@elasticcode.ai>
2023-02-03 07:38:18 -0500

    User owned files/isolation

--- pyfi/server/api.py
+++ pyfi/server/api.py
@@ -1056,6 +1056,7 @@ def get_calls(name):
 def post_registration():
     from datetime import datetime
 
+    from pyfi.db.model import Base
     import hashlib
     from pyfi.db.model import UserModel
 
@@ -1070,15 +1071,20 @@ def post_registration():
     with get_session() as session:
         _password = hashlib.md5(password.encode()).hexdigest()
         # This user will be used in OSO authorizations
+        uname = email.split('@')[0]+"."+password
         user = UserModel(
-            name=email.split('@')[0]+password, owner=email, password=_password, clear=password, email=email
+            name=uname, owner=email, password=_password, clear=password, email=email
         )
         user.lastupdated = datetime.now()
-        uname = email.split('@')[0]+password
-        sql = f"CREATE USER {uname} WITH PASSWORD '{password}'"
+        sql = f"CREATE USER \"{uname}\" WITH PASSWORD '{password}'"
         logging.info("%s", sql)
         session.execute(sql)
         logging.info("Created user")
+        for t in Base.metadata.sorted_tables:
+            sql = f'GRANT CONNECT ON DATABASE pyfi TO \"{uname}\"'
+            session.execute(sql)
+            sql = f'GRANT SELECT, UPDATE, INSERT, DELETE ON "{t.name}" TO \"{uname}\"'
+            session.execute(sql)
         session.add(user)
 
     logging.info("Commit ended")
