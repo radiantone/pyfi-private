168c7d8a290887aae3d35ee59272d812626547e4
Darren Govoni <darren@elasticcode.ai>
2023-02-11 09:48:16 -0500

    Dependencies

--- bin/depends.sh
+++ bin/depends.sh
@@ -1,8 +1,8 @@
-sudo apt-get install     ca-certificates     curl     gnupg     lsb-release
-sudo apt install python3.10-venv
-sudo apt install make
-sudo apt install gcc
-sudo apt install python3-dev
-sudo apt install libpq-dev
-sudo apt install postgresql-common
-sudo apt install libev-dev
+sudo apt install -y ca-certificates     curl     gnupg     lsb-release
+sudo apt install -y python3.10-venv
+sudo apt install -y make
+sudo apt install -y gcc
+sudo apt install -y python3-dev
+sudo apt install -y libpq-dev
+sudo apt install -y postgresql-common
+sudo apt install -y libev-dev
