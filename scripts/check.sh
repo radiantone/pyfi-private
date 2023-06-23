42bff14c2428bdde8c2f9149885d4c4c0feaf043
Darren Govoni <darren@elasticcode.ai>
2023-06-23 17:20:01 -0400

    Minor edit

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,4 +1,4 @@
 cd /home/darren/git/pyfi-private
 date >/tmp/deploy
-docker images|grep -v develop|awk '{ print "docker rmi "$3 }'|sh
+docker images|grep -v local|awk '{ print "docker rmi "$3 }'|sh
 make deploy >> /tmp/deploy 2>&1
