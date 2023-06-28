48c62cb271e12dfa81c25a6414b4d4d0e3e7e4d5
Darren Govoni <darren@elasticcode.ai>
2023-06-28 08:05:56 -0400

    Tweaks

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,4 +1,3 @@
-cd /home/darren/git/pyfi-private || exit
+cd /home/ubuntu/git/pyfi-private || exit
 date >/tmp/deploy
-docker images|grep -v develop|awk '{ print "docker rmi "$3 }'|sh
-make deploy >> /tmp/deploy 2>&1
+make stop && make refresh && make deploy
