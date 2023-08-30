1f6dd4a127c0e4c0d69db15b079d94132d74cf45
Darren Govoni <darren@elasticcode.ai>
2023-08-30 09:35:39 -0400

    EL-250 Drag and drag from app sidebar

--- ui/src/layouts/AppLayout.vue
+++ ui/src/layouts/AppLayout.vue
@@ -9,7 +9,7 @@
           flat
           dense
           round
-          @click="toggleLeftDrawer"
+        @click="leftDrawerOpen=!leftDrawerOpen"
           aria-label="Menu"
           icon="menu"
         />
@@ -30,19 +30,16 @@
         <q-item-label header>
           Essential Links
         </q-item-label>
-        <q-item
-          clickable
-          target="_blank"
-          rel="noopener"
-          href="https://quasar.dev"
+        <q-item @drag="drag" @dragend="dragend" draggable="true"
+             unselectable="on"
         >
           <q-item-section avatar>
             <q-icon name="school"/>
           </q-item-section>
           <q-item-section>
-            <q-item-label>Docs</q-item-label>
+            <q-item-label>Banner</q-item-label>
             <q-item-label caption>
-              https://quasar.dev
+              Application Image Banner
             </q-item-label>
           </q-item-section>
         </q-item>
@@ -137,16 +134,25 @@
         />
         <q-btn label="Fetch" @click="fetch"></q-btn>
       </div>
+      <LayoutTemplate ref="layout"/>
     </q-page-container>
   </q-layout>
 </template>
+<style>
 
+</style>
 <script>
 import DataService from "components/util/DataService"
+import LayoutTemplate from "components/templates/LayoutTemplate.vue"
+
+let mouseXY = {"x": null, "y": null};
+let DragPos = {"x": null, "y": null, "w": 1, "h": 1, "i": null};
 
 export default {
   name: 'AppLayout',
-
+  components: {
+    LayoutTemplate
+  },
   data() {
     return {
       leftDrawerOpen: false,
@@ -157,8 +163,77 @@ export default {
     DataService.getMock().then((res) => {
       console.log("DATA MOCK FROM APP", res)
     })
+       document.addEventListener("dragover", function (e) {
+            mouseXY.x = e.clientX;
+            mouseXY.y = e.clientY;
+        }, false);
   },
   methods: {
+    drag: function (e) {
+            let parentRect = document.getElementById('content').getBoundingClientRect();
+            let mouseInGrid = false;
+            if (((mouseXY.x > parentRect.left) && (mouseXY.x < parentRect.right)) && ((mouseXY.y > parentRect.top) && (mouseXY.y < parentRect.bottom))) {
+                mouseInGrid = true;
+            }
+            if (mouseInGrid === true && (this.$refs.layout.layout.findIndex(item => item.i === 'drop')) === -1) {
+                this.$refs.layout.layout.push({
+                    x: (this.$refs.layout.layout.length * 2) % (this.colNum || 12),
+                    y: this.$refs.layout.layout.length + (this.colNum || 12), // puts it at the bottom
+                    w: 1,
+                    h: 1,
+                    i: 'drop',
+                });
+            }
+            let index = this.$refs.layout.layout.findIndex(item => item.i === 'drop');
+            if (index !== -1) {
+                try {
+                    this.$refs.layout.$refs.gridlayout.$children[this.$refs.layout.layout.length].$refs.item.style.display = "none";
+                } catch {
+                }
+                let el = this.$refs.layout.$refs.gridlayout.$children[index];
+                el.dragging = {"top": mouseXY.y - parentRect.top, "left": mouseXY.x - parentRect.left};
+                let new_pos = el.calcXY(mouseXY.y - parentRect.top, mouseXY.x - parentRect.left);
+
+                if (mouseInGrid === true) {
+                    this.$refs.layout.$refs.gridlayout.dragEvent('dragstart', 'drop', new_pos.x, new_pos.y, 1, 1);
+                    DragPos.i = String(index);
+                    DragPos.x = this.$refs.layout.layout[index].x;
+                    DragPos.y = this.$refs.layout.layout[index].y;
+                }
+                if (mouseInGrid === false) {
+                    this.$refs.layout.$refs.gridlayout.dragEvent('dragend', 'drop', new_pos.x, new_pos.y, 1, 1);
+                    this.$refs.layout.layout = this.$refs.layout.layout.filter(obj => obj.i !== 'drop');
+                }
+            }
+        },
+        dragend: function (e) {
+            let parentRect = document.getElementById('content').getBoundingClientRect();
+            let mouseInGrid = false;
+            if (((mouseXY.x > parentRect.left) && (mouseXY.x < parentRect.right)) && ((mouseXY.y > parentRect.top) && (mouseXY.y < parentRect.bottom))) {
+                mouseInGrid = true;
+            }
+            if (mouseInGrid === true) {
+                alert(`Dropped element props:\n${JSON.stringify(DragPos, ['x', 'y', 'w', 'h'], 2)}`);
+                this.$refs.layout.$refs.gridlayout.dragEvent('dragend', 'drop', DragPos.x, DragPos.y, 1, 1);
+                this.$refs.layout.layout = this.$refs.layout.layout.filter(obj => obj.i !== 'drop');
+
+                // UNCOMMENT below if you want to add a grid-item
+                /*
+                this.$refs.layout.layout.push({
+                    x: DragPos.x,
+                    y: DragPos.y,
+                    w: 1,
+                    h: 1,
+                    i: DragPos.i,
+                });
+                this.$refs.gridLayout.dragEvent('dragend', DragPos.i, DragPos.x,DragPos.y,1,1);
+                try {
+                    this.$refs.gridLayout.$children[this.$refs.layout.layout.length].$refs.item.style.display="block";
+                } catch {
+                }
+                */
+            }
+        },
     toggleLeftDrawer() {
       leftDrawerOpen.value = !leftDrawerOpen.value
     },
