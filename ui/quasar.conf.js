582b24e2db4c94226f5e94532d56873506d94ead
Darren Govoni <darren@elasticcode.ai>
2023-09-21 11:21:06 -0400

    UI Build

--- ui/quasar.conf.js
+++ ui/quasar.conf.js
@@ -71,7 +71,7 @@ module.exports = configure(function (ctx) {
 
       // Options below are automatically set depending on the env, set them if you want to override
       // extractCSS: false,
-
+      sourceMap: true,
       // https://quasar.dev/quasar-cli/handling-webpack
       extendWebpack (cfg) {
         // linting is slow in TS projects, we execute it only for production builds
