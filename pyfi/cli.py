26cb6513c56b9fcfaa134461c05ec3c97b31bc52
Darren Govoni <darren@elasticcode.ai>
2023-10-10 09:37:29 -0400

    delete user command

--- pyfi/cli.py
+++ pyfi/cli.py
@@ -1284,7 +1284,7 @@ def delete_processor(context, name):
 )
 @click.pass_context
 def delete_user(context, email):
-    import json
+    import requests
 
     import chargebee
     from auth0.authentication import GetToken
@@ -1296,60 +1296,60 @@ def delete_user(context, email):
 
     get_token = GetToken(domain, client_id, client_secret=client_secret)
     token = get_token.client_credentials("https://{}/api/v2/".format(domain))
-    print(token)
     auth0 = Auth0(domain, token["access_token"])
 
     users = auth0.users.list()
-    print(users)
     """
     user_id = '{YOUR_USER_ID}'
 auth0.users.update(user_id, {
     'name': 'My name is...'
 })"""
-
+    print(users)
     chargebee.configure(os.environ["CB_KEY"], os.environ["CB_SITE"])
     user = (
         context.obj["database"].session.query(UserModel).filter_by(email=email).first()
     )
 
     if user is None:
-        print("No such user")
-        return
-
-    print(f"Deleting {email}")
-    print(user)
-    try:
-        context.obj["database"].session.execute(f'DROP OWNED BY "{user.name}"')
-    except:
-        pass
-    try:
-        context.obj["database"].session.execute(f'DROP USER "{user.name}"')
-    except:
-        pass
-
-    context.obj["database"].session.delete(user)
-
-    result = chargebee.Customer.list({"email[is]": user.email})
-    if len(result) == 0:
-        print(f"No customer for {id}")
+        print(f"No such user {email}")
     else:
-        customer_id = result[0].customer.id
-        print(f"Deleting chargebee customer...{customer_id}")
+        print(f"Deleting {email}")
         try:
-            # result = chargebee.Customer.delete(customer_id)
-            print(f"Deleted chargebee customer...{customer_id}")
-        except Exception as ex:
-            # Only absorb exception if its a "no user found" exception
+            context.obj["database"].session.execute(f'DROP OWNED BY "{user.name}"')
+        except:
+            pass
+        try:
+            context.obj["database"].session.execute(f'DROP USER "{user.name}"')
+        except:
             pass
 
-    print("Deleting Auth0 user...")
+        context.obj["database"].session.delete(user)
+
+    print("Checking subscriptions")
+    result = chargebee.Customer.list({"email[is]": email})
+    if len(result) == 0:
+        print(f"No customer with email {email}")
+    else:
+        for customer in result:
+            customer_id = customer.customer.id
+            print(f"Deleting chargebee customer...{email} {customer_id}")
+            try:
+                result = chargebee.Customer.delete(customer_id)
+                print(f"Deleted chargebee customer...{email} {customer_id}")
+            except Exception as ex:
+                # Only absorb exception if its a "no user found" exception
+                pass
+
+    print("Checking Auth0...")
     try:
-        # Delete Auth0 user
-        # curl -L -X DELETE 'https://login.auth0.com/api/v2/users/:id'
-        print("Deleted Auth0 user...")
+        for user in users['users']:
+            if user['email'] == email:
+                print(f"Deleting Auth0 user...{email}")
+                auth0.users.delete(user['user_id'])
+                print(f"Deleted Auth0 user...{email}")
+
     except Exception as ex:
-        # Only absorb exception if its a "no user found" exception
-        pass
+        print(ex)
 
     context.obj["database"].session.commit()
     print("Deleted user", id)
